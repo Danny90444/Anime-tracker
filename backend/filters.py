fc78e2eeda88a72593afeff1e262a7fa12102430
Danny90444 <90482863+Danny90444@users.noreply.github.com>
2026-02-13 11:54:11 -0500

    Update filters.py

--- backend/filters.py
+++ backend/filters.py
@@ -1,1 +1,13 @@
 #ilter logic 
+
+'''If genres is None → return list
+
+Normalize case
+
+If match_mode == "any":
+keep anime if at least one matches
+
+If match_mode == "all":
+keep anime if all selected genres are present
+
+'''
